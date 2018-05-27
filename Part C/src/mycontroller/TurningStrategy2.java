b1d1bb4f5305ff014158734f9c435ff85a088e59
Unknown <1578942650@qq.com>
2018-05-27 15:51:07 +1000

    Wall adjustment working

--- Part C/src/mycontroller/TurningStrategy2.java
+++ Part C/src/mycontroller/TurningStrategy2.java
@@ -1,30 +1,32 @@
 package mycontroller;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
+import tiles.MapTile;
+import utilities.Coordinate;
 import world.Car;
+import world.WorldSpatial.Direction;
 
 public class TurningStrategy2 implements TurningStrategy{
 	private MyAIController car;
+	private int wallSensitivity = 4;
+	public enum WallPosition {FRONT, FOLLOWING, NOWALL};
 	public TurningStrategy2(MyAIController car) {
 		this.car = car;
 	}
 	
 	@Override
 	public void turn(float delta, int absoluteDegree) {
-		if (this.car.getSpeed() > 0.5) {
-			System.out.println("stopppppppppppp!!");
-			
-			try {
-				TimeUnit.SECONDS.sleep(1);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+		if (this.car.getSpeed() > 0.11) {			
 			this.car.applyBrake();
 		}else {
-			this.turnToDirection(this.car.getAngle(), delta, absoluteDegree);
+			if (adjust()) {
+				System.out.println("adjust");
+				return;
+			} else {
+				this.turnToDirection(this.car.getAngle(), delta, absoluteDegree);
+			}
 		}
 	}
 	
@@ -35,10 +37,7 @@ public class TurningStrategy2 implements TurningStrategy{
 				this.car.applyBrake();
 			} else {
 				this.car.applyReverseAcceleration();
-			}
-			
-			
-			
+			}			
 			if ((currentDegree - absoluteDegree) >= 0 ) {
 				if (Math.abs(currentDegree - absoluteDegree) <= Math.abs(360 - currentDegree + absoluteDegree)) {
 					this.car.turnLeft(delta);
@@ -56,5 +55,191 @@ public class TurningStrategy2 implements TurningStrategy{
 			this.car.applyBrake();
 		}
 	}
-
+	
+	
+	private boolean adjust() {
+		WallPosition wallPosition = checkWall();
+		System.out.println("WallPosition: " + wallPosition);
+		switch (wallPosition) {
+		case FRONT:
+			return frontWallAdjust();
+		case FOLLOWING:
+			return backWallAdjust();
+		default:
+			return false;
+		}
+	}
+	
+	private boolean frontWallAdjust() {
+		Direction currentDirection = this.car.getOrientation();
+		Coordinate currentPos = new Coordinate(this.car.getPosition());
+		float x = this.car.getX();
+		float y = this.car.getY();
+		switch (currentDirection){
+		case WEST:
+			if (currentPos.x > x) {
+				this.car.applyReverseAcceleration();
+				return true;
+			}
+			return false;
+		case EAST:
+			if (currentPos.x < x) {
+				this.car.applyReverseAcceleration();
+				return true;
+			}
+			return false;
+		case SOUTH:
+			if (currentPos.y > y) {
+				this.car.applyReverseAcceleration();
+				return true;
+			}
+			return false;
+		case NORTH:
+			if (currentPos.y < y){
+				this.car.applyReverseAcceleration();
+				return true;
+			}
+			return false;
+		default:
+			return false;
+		}
+	}
+	
+	private boolean backWallAdjust() {
+		Direction currentDirection = this.car.getOrientation();
+		Coordinate currentPos = new Coordinate(this.car.getPosition());
+		float x = this.car.getX();
+		float y = this.car.getY();
+		switch (currentDirection){
+		case WEST:
+			if (currentPos.x > x) {
+				this.car.applyReverseAcceleration();
+				return true;
+			}
+			return false;
+		case EAST:
+			if (currentPos.x < x) {
+				this.car.applyReverseAcceleration();
+				return true;
+			}
+			return false;
+		case SOUTH:
+			if (currentPos.y < y) {
+				this.car.applyReverseAcceleration();
+				return true;
+			}
+			return false;
+		case NORTH:
+			if (currentPos.y > y){
+				this.car.applyReverseAcceleration();
+				return true;
+			}
+			return false;
+		default:
+			return false;
+		}
+	}
+	
+	private WallPosition checkWall() {
+		Direction currentDirection = this.car.getOrientation();
+		HashMap<Coordinate, MapTile> currentView = this.car.getView();
+		switch (currentDirection) {
+		case EAST:
+			if (checkEast(currentView)) {
+				return WallPosition.FRONT;
+			} else if (checkWest(currentView)){
+				return WallPosition.FOLLOWING;
+			} else {
+				return WallPosition.NOWALL;
+			}
+		case WEST:
+			if (checkEast(currentView)) {
+				return WallPosition.FOLLOWING;
+			} else if (checkWest(currentView)){
+				return WallPosition.FRONT;
+			} else {
+				return WallPosition.NOWALL;
+			}
+		case SOUTH:
+			if (checkSouth(currentView)) {
+				return WallPosition.FRONT;
+			} else if (checkNorth(currentView)){
+				return WallPosition.FOLLOWING;
+			} else {
+				return WallPosition.NOWALL;
+			}
+		case NORTH:
+			if (checkNorth(currentView)) {
+				return WallPosition.FRONT;
+			} else if (checkSouth(currentView)){
+				return WallPosition.FOLLOWING;
+			} else {
+				return WallPosition.NOWALL;
+			}
+		default:
+			return WallPosition.NOWALL;
+		}
+	}
+	
+	/**
+	 * Method below just iterates through the list and check in the correct coordinates.
+	 * i.e. Given your current position is 10,10
+	 * checkEast will check up to wallSensitivity amount of tiles to the right.
+	 * checkWest will check up to wallSensitivity amount of tiles to the left.
+	 * checkNorth will check up to wallSensitivity amount of tiles to the top.
+	 * checkSouth will check up to wallSensitivity amount of tiles below.
+	 */
+	public boolean checkEast(HashMap<Coordinate, MapTile> currentView){
+		// Check tiles to my right
+		Coordinate currentPosition = new Coordinate(this.car.getPosition());
+		for (Coordinate coor: currentView.keySet()) {
+			if (coor.x == currentPosition.x+1) {
+				MapTile tile = currentView.get(coor);
+				if(tile.isType(MapTile.Type.WALL)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public boolean checkWest(HashMap<Coordinate,MapTile> currentView){
+		// Check tiles to my left
+		Coordinate currentPosition = new Coordinate(this.car.getPosition());
+		for (Coordinate coor: currentView.keySet()) {
+			if (coor.x == currentPosition.x-1) {
+				MapTile tile = currentView.get(coor);
+				if(tile.isType(MapTile.Type.WALL)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public boolean checkNorth(HashMap<Coordinate,MapTile> currentView){
+		Coordinate currentPosition = new Coordinate(this.car.getPosition());
+		for (Coordinate coor: currentView.keySet()) {
+			if (coor.y == currentPosition.y+1) {
+				MapTile tile = currentView.get(coor);
+				if(tile.isType(MapTile.Type.WALL)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public boolean checkSouth(HashMap<Coordinate,MapTile> currentView){
+		Coordinate currentPosition = new Coordinate(this.car.getPosition());
+		for (Coordinate coor: currentView.keySet()) {
+			if (coor.y == currentPosition.y-1) {
+				MapTile tile = currentView.get(coor);
+				if(tile.isType(MapTile.Type.WALL)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
